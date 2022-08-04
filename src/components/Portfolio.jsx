eae6f4ba7b4e351c7fe1c99a75e4bdcdf4e297a2
teaguesibert <90118021+teaguesibert@users.noreply.github.com>
2022-08-04 01:52:56 -0400

    added titles for portfolio projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,35 +11,41 @@ const Portfolio = () => {
     {
       id: 1,
       src: spotifyRemote,
+      title:'Spotify Remote Player',
       demo: 'https://www.youtube.com/watch?v=VzeghVTIGGE',
       code: 'https://github.com/teaguesibert/spotify-remote-player-tailwind',
     },
     {
       id: 2,
       src: movieSearch,
+      title:'Movie Search Tool',
       demo: 'https://moviesearchts.netlify.app/',
       code: 'https://github.com/teaguesibert/movie-search',
     },
     {
       id: 3,
       src: saltX,
+      title:'Godot Gamejam Game',
       demo: 'https://onionknight.itch.io/salt-x',
       code: 'https://onionknight.itch.io/salt-x',
     },
     {
       id: 4,
       src: trailerCompany,
+      title:'Trailer Company Site',
       demo: 'https://trailer-company.netlify.app/',
       code: 'https://github.com/teaguesibert/trailer-company-site',
     },
     {
       id: 5,
       src: portfolio,
+      title:'HTML Portfolio',
       demo: 'https://html-portfolio-ts.netlify.app/',
       code: 'https://github.com/teaguesibert/portfolio-html',
     },
     {
       id: 6,
+      title:'Ten-Second Math Game',
       src: mathGame,
       demo: 'https://tensecondmathgamets.netlify.app/',
       code: 'https://github.com/teaguesibert/10secmathgame',
@@ -60,13 +66,14 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, code, demo }) => (
+          {portfolios.map(({ id, src, code, demo, title }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
+              <div className="pt-2 text-white text-large text-center">{title}</div>
               <div className="flex items-center justify-center">
                 <a className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-gray-400 text-center" href={demo} rel="noopener noreferrer" target="_blank">
                   Demo
